57b0dc0d81d1d03a7f9449ad9a35f7e7c9241841
ianpara <imp2468@gmail.com>
2019-09-26 18:03:36 -0400

    server setup
    add port in app.py
    removed wsgi req

--- app.py
+++ app.py
@@ -152,4 +152,5 @@ def logout():
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug="true", ssl_context='adhoc')

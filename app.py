729a01a2629124362df55d8097f1a1da66277935
ianpara <imp2468@gmail.com>
2019-09-26 10:49:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -152,4 +152,4 @@ def logout():
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True, ssl_context="adhoc")
+    app.run(debug=True, ssl_context="adhoc")

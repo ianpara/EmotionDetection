cea7240dc7b5b456417b36d858eb4dd8f4708876
YasminGad <yasmin.gad@gmail.com>
2019-10-18 20:22:25 -0400

    Added more db methods
    Main changes in db_liteconnect and sqlite_db

--- database/db_liteconnect.py
+++ database/db_liteconnect.py
@@ -23,7 +23,7 @@ class Database():
 
     #selet a single user based on the passed in ID
     def select_a_user(givenID):
-        cursor.execute("SELECT * FROM user where id = ?", [givenID]) #requires list literal [ ... ]
+        cursor.execute("SELECT * FROM user where userID = ?", [givenID]) #requires list literal [ ... ]
         data = cursor.fetchall()
         return data
 
@@ -31,8 +31,8 @@ class Database():
     # takes in three parameters that contains that user's info
     #    !!!   not really working without auto increment.
     @staticmethod
-    def add_user(addID, addName, addEmail, addPic):
-        cursor.execute("INSERT INTO user(id, name, Email, profile_pic) VALUES (?, ?, ?, ?)", (addID, addName, addEmail, addPic))
+    def add_user(addName, addEmail, addPic):
+        cursor.execute("INSERT INTO user(name, Email, profile_pic) VALUES (?, ?, ?, ?)", (addName, addEmail, addPic))
         dbconn.commit()
 
     # removes a user when given username
@@ -57,14 +57,14 @@ class Database():
     @staticmethod
     #spew forth all cheesiness
     def select_all_jokes():
-        cursor.execute("SELECT * FROM user")
+        cursor.execute("SELECT * FROM jokes")
         data = cursor.fetchall()
         return data
 
     @staticmethod
     #select a single joke based on the joke's ID
     def select_a_joke(givenJokeID):
-        cursor.execute("SELECT * FROM user where id = ?", [givenJokeID]) #requires list literal [ ... ]
+        cursor.execute("SELECT * FROM jokes where jokeID = ?", [givenJokeID]) #requires list literal [ ... ]
         data = cursor.fetchall()
         return data
 
@@ -84,3 +84,53 @@ class Database():
     def remove_jokeID(removeJokeID):
         cursor.execute("DELETE FROM jokes WHERE jokeID = ?", [removeJokeID])
         dbconn.commit()
+
+############ E N D __ J O K E __ M E T H O D S ############
+
+############ B E G I N __ J O K E __ T R A C K E R __ M E T H O D S ############
+    @staticmethod
+    def add_joke_tracked(userID, jokeID):
+        cursor.execute("INSERT INTO joke_tracker (userID, jokeID) VALUES (?, ?)", [userID], [jokeID])
+        dbconn.commit()
+
+
+    #@staticmethod
+    #def search_joke_tracked(userID, jokeID):
+        # TO DO
+############ E N D __ J O K E __ T R A C K E R __ M E T H O D S ############
+
+
+############ B E G I N __ M O T I V A T I O N A L __ M E T H O D S ############
+
+    @staticmethod
+    #spew forth all cheesiness
+    def select_all_motivationals():
+        cursor.execute("SELECT * FROM motivational_quotes")
+        data = cursor.fetchall()
+        return data
+
+    @staticmethod
+    #select a single joke based on the joke's ID
+    def select_a_motivational(givenMotivationalID):
+        cursor.execute("SELECT * FROM motivational_quotes where motivationalID = ?", [givenMotivationalID]) #requires list literal [ ... ]
+        data = cursor.fetchall()
+        return data
+
+
+    @staticmethod
+    def add_motivational(newMotivationalText):
+        cursor.execute("INSERT INTO motivational_quotes (motivational) VALUES (?)", [newMotivationalText])
+        dbconn.commit()
+
+
+    @staticmethod
+    def remove_joke_text(removeJokeText):
+        cursor.execute("DELETE FROM motivational_quotes WHERE motivational = ?", [removeJokeText])
+        dbconn.commit()
+
+    @staticmethod
+    def remove_jokeID(removeJokeID):
+        cursor.execute("DELETE FROM motivational_quotes WHERE motivationalID = ?", [removeJokeID])
+        dbconn.commit()
+
+############ E N D __ M O T I V A T I O N A L __ M E T H O D S ############
